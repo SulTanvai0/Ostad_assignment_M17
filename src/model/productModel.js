09eb6a8666b860cfffb5944a0ab5d37579270b12
SulTanvai0 <MuhammedMehedih@gmail.com>
2023-12-09 18:46:18 +0600

    frist commit

---
+++ src/model/productModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+const productSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true, min: 1 },
+    category: { type: String, required: true },
+    imageURL: { type: String }, 
+}, { timestamps: true, versionKey: false } );
+
+const productModel = mongoose.model('products', productSchema);
+
+module.exports = productModel;
