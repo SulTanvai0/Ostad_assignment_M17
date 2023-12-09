aa439e17fa74b1e144c6dcbca074a1e2de10429e
SulTanvai0 <MuhammedMehedih@gmail.com>
2023-12-09 22:03:35 +0600

    fixing model issue

--- src/model/productModel.js
+++ src/model/productModel.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
-
 const productSchema = mongoose.Schema({
     name: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String },
     price: { type: Number, required: true, min: 1 },
+    stock: { type: Number, required: true, min: 0, validate: Number.isInteger },
     category: { type: String, required: true },
-    imageURL: { type: String }, 
-}, { timestamps: true, versionKey: false } );
+}, { timestamps: true, versionKey: false });
 
-const productModel = mongoose.model('products', productSchema);
+const productModel = mongoose.model('Product', productSchema);
 
 module.exports = productModel;
+
